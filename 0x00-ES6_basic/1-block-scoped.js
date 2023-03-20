332707d0b833fb37d7a4de917670ef252b65ce79
omi2v <omibedewi@gmail.com>
2023-03-20 20:00:48 +0000

    block scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
-var task = false;
 export default function taskBlock(trueOrFalse) {
-  var task2 = true;
+  var task = false;
+  var task2 = false;
 
   if (trueOrFalse) {
     var task = true;
-    var task2 = false;
+    var task2 = true;
   }
 
   return [task, task2];
