e0f5adb66916bc794e402e3ece8008148f94a321
omi2v <omibedewi@gmail.com>
2023-03-28 12:45:03 +0000

    typed array

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,11 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
-  const int8Array = new Dataview(buffer);
+  const val = new DataView(buffer);
 
   try {
-    int8Array.setInt8(position, value);
+    val.setInt8(position, value);
   } catch (e) {
     throw Error('position outside range');
   }
-  return int8Array;
+  return val;
 }
