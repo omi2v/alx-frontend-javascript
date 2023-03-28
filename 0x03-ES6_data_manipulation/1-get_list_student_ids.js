19a03a737c1a2620dca2f4f474b9ea7729d94da4
omi2v <omibedewi@gmail.com>
2023-03-28 11:41:54 +0000

    list 2

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,5 +2,5 @@ export default function getListStudentIds(student) {
   if (!Array.isArray(student)) {
     return [];
   }
-  return student.map((student) => student.id);
+  return student.map((obj) => obj.id);
 }
